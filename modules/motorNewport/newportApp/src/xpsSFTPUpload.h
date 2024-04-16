5c5e9b0cd90c33d7c16480e45f58348b2413bc53
esmithExperimentControls <evan.smith@stfc.ac.uk>
2024-04-16 16:07:32 +0100

    Import motor version R7-3-1

--- modules/motorNewport/newportApp/src/xpsSFTPUpload.h
+++
@@ -1,4 +0,0 @@
-#include <string>
-
-epicsShareFunc int xpsSFTPUpload(std::string IPAddress, std::string trajectoryDirectory, std::string fileName, 
-                                 std::string userName, std::string password, bool verbose);
